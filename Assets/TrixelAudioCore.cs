62b5a717489548e89af3ff7707faa992dcb43772
acidiclight <alkalinethunder@gmail.com>
2022-10-31 20:58:52 -0400

    Initial Unity project

---
+++ Assets/TrixelAudioCore.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace TrixelCreative.TrixelAudio
+{
+    public class TrixelAudioCore : MonoBehaviour
+    {
+        private void Awake()
+        {
+            Debug.Log("Hello world!");
+        }
+    }
+}
