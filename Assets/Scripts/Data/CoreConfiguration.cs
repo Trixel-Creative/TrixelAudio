696b364ef13a808a12a6ea0104221530d32dff1b
acidiclight <alkalinethunder@gmail.com>
2022-11-02 23:18:03 -0400

    Add audio mixer groups to CoreConfiguration

--- Assets/Scripts/Data/CoreConfiguration.cs
+++ Assets/Scripts/Data/CoreConfiguration.cs
@@ -1,13 +1,33 @@
 ﻿using UnityEngine;
+using UnityEngine.Audio;
 
 namespace TrixelCreative.TrixelAudio.Data
 {
 	[CreateAssetMenu(menuName = "TrixelAudio/Core Configuration")]
 	public class CoreConfiguration : ScriptableObject
 	{
+		[Header("Memory management")]
 		[SerializeField]
 		[Tooltip("Determines how many audio sources are allocated in the scene for sound effects. Effectively this determines how many sound effects can be played at once.")]
 		private int soundEffectPoolSize = 50;
+
+		[Header("Audio mixers")]
+		[SerializeField]
+		private AudioMixerGroup soundEffectsGroup = null!;
+
+		[SerializeField]
+		private AudioMixerGroup musicGroup = null!;
+
+		[SerializeField]
+		private AudioMixerGroup ambienceGroup = null!;
+
+		[SerializeField]
+		private AudioMixerGroup dialogueGroup = null!;
+
+		public AudioMixerGroup SoundEffectsMixer => soundEffectsGroup;
+		public AudioMixerGroup MusicMixer => musicGroup;
+		public AudioMixerGroup AmbienceMixer => ambienceGroup;
+		public AudioMixerGroup DialogueMixer => dialogueGroup;
 
 		public int SoundEffectPoolSize => soundEffectPoolSize;
         
