002c538c286a4097e633beb7e4bc5769ad114c04
acidiclight <alkalinethunder@gmail.com>
2022-11-05 17:28:37 -0400

    Add support for SoundEvents

---
+++ Assets/Scripts/Players/SoundEvent.cs
@@ -0,0 +1,17 @@
+﻿using TrixelCreative.TrixelAudio.Data;
+using UnityEngine;
+
+namespace TrixelCreative.TrixelAudio.Players
+{
+	public class SoundEvent : AudioPlayerBase
+	{
+		[SerializeField]
+		private SoundEffectAsset? soundEffect;
+
+		public void Play()
+		{
+			if (soundEffect != null)
+				AudioSource.Play(soundEffect);
+		}
+	}
+}
